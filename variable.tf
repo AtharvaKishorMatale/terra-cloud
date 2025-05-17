86a67be94489c89355d4271b5a141f35df32061e
Atharva Kishor Matale <160848806+AtharvaKishorMatale@users.noreply.github.com>
2025-05-17 12:06:18 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,15 @@
+#variables
+variable "cidr_block"{
+    default = "10.0.0.0/16"
+}
+
+variable "web-cidr_block"{
+     default = "10.0.0.0/20"
+}
+variable "app-cidr_block"{
+      default = "10.0.16.0/20"
+}
+variable "db-cidr_block"{
+     default = "10.0.32.0/20"
+
+}
